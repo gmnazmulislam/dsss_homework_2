4fa8b69d0afd1e6bf0e0a4ce483d003ff0c39869
gmnazmulislam <150603887+gmnazmulislam@users.noreply.github.com>
2023-11-12 20:05:01 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,46 @@
 import random
 
-def generate_random_integer(min_value, max_value):
+def generate_random_integer(min_val, max_val):
     """
     Generating a random integer between a given minimum and maximum value
     """
-    return random.randint(min_value, max_value)
+    return random.randint(min_val, max_val)
 
 def generate_operator():
     """
-    Generating a random arithmetic operator: +/ - / *
+    Generating an arithmetic operator: +/ - / *
     """
     return random.choice(['+', '-', '*'])
 
-def evaluate_expression(number1, number2, operator):
+def evaluate_problem(num1, num2, operator):
     """
-    Evaluate the expression based on provided numbers and operator
+    Evaluate the problem based on the provided numbers and operator
     """
-    expression = f"{number1} {operator} {number2}"
+    problem = f"{num1} {operator} {num2}"
     if operator == '+': 
-        answer = number1 + number2
+        ans = num1 + num2
     elif operator == '-': 
-        answer = number1 - number2
+        ans = num1 - num2
     else:
-        answer = number1 * number2
-    return expression, answer
+        ans = num1 * num2
+    return problem, ans
 
 def math_quiz():
     """
     A math quiz game where users have to answer arithmetic questions.
     """
     score = 0
-    total_questions = 3  # change float to integer as no of question can't be fraction.
+    total_questions = 3  # change float to integer as no of question can't be a fraction.
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(total_questions):
-        num1 = generate_random_integer(1, 10)
-        num2 = generate_random_integer(1, 5)  # change 5.5 to 5 as randint does not support float
+        number1 = generate_random_integer(1, 10)
+        number2 = generate_random_integer(1, 5)  # change 5.5 to 5 as randint does not support float
         operator = generate_operator()
 
-        problem, answer = evaluate_expression(num1, num2, operator)
+        problem, ans = evaluate_problem(number1, number2, operator)
         print(f"\nQuestion: {problem}")
         try:
             user_answer = int(input("Your answer: "))
@@ -48,11 +48,11 @@ def math_quiz():
             print("Invalid input")
             continue
 
-        if user_answer == answer:
+        if user_answer == ans:
             print("Correct! You earned a point.")
             score += 1
         else:
-            print(f"Wrong answer. The correct answer is {answer}.")
+            print(f"Wrong answer. The correct answer is {ans}.")
 
     print(f"\nGame over! Your score is: {score}/{total_question}")
 
