fa220bdf69a7c3a5b91ad22f06a9857506629631
gmnazmulislam <150603887+gmnazmulislam@users.noreply.github.com>
2023-11-12 20:21:11 +0100

    Update tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,25 +1,25 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_integer, generate_operator, calculate_problem
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_integer(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
+    def test_generate_operator(self):
         Valid_operators= ['+','-','*']
         for _ in range(1000):
-            operator= function_B()
+            operator= generate_operator()
             self.assertIn(operator, valid_operators)
         
 
-    def test_function_C(self):
+    def test_calculate_problem(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
                 (8, 3, '-', '8 - 3', 5),
